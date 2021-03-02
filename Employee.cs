671c78effcdcdf6df05d626e5e14e25ce03b2878
DamonFlick <75501983+DamonFlick@users.noreply.github.com>
2021-03-01 16:51:08 -0800

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambdas_1
+{
+    class Employee : Person
+    {
+        public int ID { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambdas_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee() { firstName = "Martha", lastName = "Stewart", ID = 1 });
+            employees.Add(new Employee() { firstName = "Joe", lastName = "Hill", ID = 2 });
+            employees.Add(new Employee() { firstName = "Bex", lastName = "Jones", ID = 3 });
+            employees.Add(new Employee() { firstName = "Julie", lastName = "Rose", ID = 4 });
+            employees.Add(new Employee() { firstName = "Joe", lastName = "Stewart", ID = 5 });
+            employees.Add(new Employee() { firstName = "Savannah", lastName = "Broma", ID = 6 });
+            employees.Add(new Employee() { firstName = "David", lastName = "Ollessies", ID = 7 });
+            employees.Add(new Employee() { firstName = "Kawika", lastName = "Ohele", ID = 8 });
+            employees.Add(new Employee() { firstName = "John", lastName = "Stewart", ID = 9 });
+            employees.Add(new Employee() { firstName = "Hugh", lastName = "Mungous", ID = 10 });
+
+
+            List<Employee> newList = new List<Employee>();
+            foreach (Employee employee in employees)
+            {
+                if (employee.firstName == "Joe")
+                {
+                    newList.Add(employee);
+                }          
+            }
+
+            List<Employee> newList2 = employees.Where(e => e.firstName == "Joe").ToList();
+            List<Employee> newList3 = employees.Where(e => e.ID > 5).ToList();
+
+            newList.ForEach(e => Console.WriteLine(e.firstName));
+            newList2.ForEach(e => Console.WriteLine(e.firstName));
+            newList3.ForEach(e => Console.WriteLine(e.firstName + e.ID));
+            Console.ReadLine();
+         }
+    }
+}
